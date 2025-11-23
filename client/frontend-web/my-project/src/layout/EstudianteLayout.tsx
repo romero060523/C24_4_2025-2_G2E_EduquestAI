75d4e6b48f542d6ab0701d4656e81b2c6fdd8584
Romero Poma Andy Jheremy <andy.romero@tecsup.edu.pe>
2025-11-22 19:07:12 -0500

    feat(frontend): integrar CharWidget con autenticacion en los layouts

--- client/frontend-web/my-project/src/layout/EstudianteLayout.tsx
+++ client/frontend-web/my-project/src/layout/EstudianteLayout.tsx
@@ -1,15 +1,24 @@
-// src/layouts/ProfesorLayout.tsx
+// src/layouts/EstudianteLayout.tsx
 import React from "react";
 import SidebarEstudiante from "../components/estudiante/SidebarEstudiante";
 import { Outlet } from "react-router-dom";
+import ChatWidget from "../components/ChatWidget";
+import { useAuth } from "../hooks/useAuth";
 
-const EstudianteLayout: React.FC = () => (
-  <div className="flex min-h-screen bg-gray-100">
-    <SidebarEstudiante />
-    <main className="flex-1 p-8">
-      <Outlet />
-    </main>
-  </div>
-);
+const EstudianteLayout: React.FC = () => {
+  const { usuario } = useAuth();
+
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <SidebarEstudiante />
+      <main className="flex-1 p-8">
+        <Outlet />
+      </main>
+      {usuario && (
+        <ChatWidget userId={usuario.id} userRole={usuario.rol} />
+      )}
+    </div>
+  );
+};
 
 export default EstudianteLayout;
--- client/frontend-web/my-project/src/layout/ProfesorLayout.tsx
+++ client/frontend-web/my-project/src/layout/ProfesorLayout.tsx
@@ -2,15 +2,24 @@
 import React from "react";
 import SidebarProfesor from "../components/profesor/SidebarProfesor";
 import { Outlet } from "react-router-dom";
+import ChatWidget from "../components/ChatWidget";
+import { useAuth } from "../hooks/useAuth";
 
-const ProfesorLayout: React.FC = () => (
-  <div className="flex min-h-screen bg-gray-100">
-    <SidebarProfesor />
-    <main className="flex-1 p-8">
-      {/* El componente cargado dinámicamente */}
-      <Outlet />
-    </main>
-  </div>
-);
+const ProfesorLayout: React.FC = () => {
+  const { usuario } = useAuth();
+
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <SidebarProfesor />
+      <main className="flex-1 p-8">
+        {/* El componente cargado dinámicamente */}
+        <Outlet />
+      </main>
+      {usuario && (
+        <ChatWidget userId={usuario.id} userRole={usuario.rol} />
+      )}
+    </div>
+  );
+};
 
 export default ProfesorLayout;
